24caf970f1d6bfa2217b76ff2113e7ef5eff9d35
shinjaehyeop <tlswoguq767@gmail.com>
2025-09-22 15:37:00 +0900

    sda
    asd

--- helloworld3/helloworld3/main.cpp
+++ helloworld3/helloworld3/main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	printf("hello, wolrd! \n");
-	return 0;
+int age = 25;
+unsigned int count = 3000000000;
+float score = 93.5f;
+const char* title = "Besic Constants";
+const char* name = "jaehyeop";
+char initial = name[0];
+printf("=== %s ===\n Name \t : %s\n Initial : %c\n Age : %d\n Score \t : %.6f\n Count \t : %u\n", title, name, initial, age, score, count);
+return 0;
 }
