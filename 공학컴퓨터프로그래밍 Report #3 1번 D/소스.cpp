f69fea7484476ef9363cb5c0ab4f75767f95ccc4
Shinjaehyeop <tlswoguq767@gmail.com>
2025-11-04 00:35:30 +0900

    Report #3 1번 D
    Report #3 1번 D

---
+++ 공학컴퓨터프로그래밍 Report #3 1번 D/소스.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main(void) {
+
+    for (int i = 1; i <= 10; i++) {
+
+        if (i > 5) {
+
+            break;
+
+        }
+
+        printf("%d ", i);
+
+    }
+
+    return 0;
+
+}
